5d56d63bbc55539deb7e8e986086e81c1f1b6fe1
Home’s Journey Book <hometavern113@gmail.com>
2026-06-23 16:46:48 +0700

    Update abilities.js

--- js/abilities.js
+++ js/abilities.js
@@ -56,6 +56,7 @@ function getAbilities(card){
       case 'on_any_death_base':    ab.push({timing:'on_any_death',effect:'hp_base',val}); break;
       case 'on_play_creature':     ab.push({timing:'on_play_creature',effect:'hp_base',val}); break;
       case 'sacrifice':            ab.push({timing:'active',effect:'sacrifice'}); break;
+      case 'invisible':            ab.push({timing:'passive',effect:'invisible'}); break;
       case 'world_maxhp':          ab.push({timing:'on_turn',effect:'world_maxhp',val}); break;
       case 'raise':              ab.push({timing:'on_turn',effect:'raise',val}); break;
       case 'rage':         ab.push({timing:'on_attack',effect:'rage',val}); break;
