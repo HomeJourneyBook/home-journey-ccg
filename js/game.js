683ed2550ebaed3e8bb2000108afd5cb661fe1ae
Home’s Journey Book <hometavern113@gmail.com>
2026-06-23 17:12:57 +0700

    Update game.js

--- js/game.js
+++ js/game.js
@@ -221,8 +221,8 @@ function doAttack(att,target){
 
   lg(`⚔ ${att.name} attacks ${target.name}!`,'imp');
   dmgCard(target,atk,oppK);
-  // No counter if attacker is invisible, target is invisible, or target is feared
-  if(!hasTag(att,'invisible')&&!hasTag(target,'invisible')&&!target.feared)
+  // No counter if target is invisible or feared
+  if(!hasTag(target,'invisible')&&!target.feared)
     dmgCard(att,target.atk+(target.atkBonus||0)+(target.rageBonus||0)+(target.squadAtkBonus||0),curK);
 
   // on_attack abilities
