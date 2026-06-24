7c6be0b3d44d881b56b6632defe92d8b1a16c18b
Home’s Journey Book <hometavern113@gmail.com>
2026-06-24 23:37:25 +0700

    Update render.js

--- js/render.js
+++ js/render.js
@@ -215,7 +215,8 @@ function mkEl(card,zone){
       <div class="card-hp-box"><span class="card-hp"><img src="./img/heart.png" class="stat-icon">${card.maxHp}</span></div>
         <img src="img/chel.png" class="card-stats-icon">
       <div class="card-atk-box"><span class="card-atk"><img src="./img/attack.png" class="stat-icon">${card.atk+(card.atkBonus||0)+(card.rageBonus||0)+(card.squadAtkBonus||0)}</span></div>
-    </div>`:''}
+    </div>`
+      :`<div class="card-stats" style="justify-content:center;"><img src="img/chel.png" class="card-stats-icon"></div>`}
     <div class="card-ability-box"><div class="card-ability">${card.ab}</div></div>`;
   if(card.id===G.previewCard&&zone==='hand'){
     d.classList.add('previewed');
