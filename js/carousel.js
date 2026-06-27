07b05be93a8b682ae2f2bd30f8e0556b9db80647
Home’s Journey Book <hometavern113@gmail.com>
2026-06-28 04:21:45 +0700

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -402,7 +402,6 @@
         #playerHandZone #jeetHand .card.previewed {
         transform: translateY(calc(var(--card-h) * -0.34)) scale(1.2) !important;
           transition: transform 0.15s, opacity 0.15s !important;
-          transform: translateY(calc(var(--card-h) * -0.34)) scale(1.05) !important;
           z-index: 2000 !important;
           opacity: 1 !important;
         }
