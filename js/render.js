14bbe63713ca5660e19957c077d9ac7e2f9c579c
Home’s Journey Book <hometavern113@gmail.com>
2026-06-30 23:45:29 +0700

    Update render.js

--- js/render.js
+++ js/render.js
@@ -169,10 +169,10 @@ const tagIcons=(card.tags||[])
     <div class="card-small-name-box"><div class="card-small-name">${card.name}</div></div>
 ${!isSW?`<div class="card-small-stats">
   <div class="card-small-hp-box"><span class="card-small-hp">${card.hp}</span></div>
-  <div class="stat-img"></div>
+  <img src="img/chel.png" class="card-stats-icon">
   <div class="card-small-atk-box"><span class="card-small-atk">${card.atk+(card.atkBonus||0)+(card.rageBonus||0)+(card.squadAtkBonus||0)}</span></div>
 </div>`
-:`<div class="card-small-stats" style="justify-content:center;"><div class="stat-img"></div></div>`}`;
+:`<div class="card-small-stats" style="justify-content:center;"><img src="img/chel.png" class="card-stats-icon"></div>`}`;
   if(card.id===G.sel&&card.f===G.turn&&!card.exhausted&&!card.sleeping&&!card.feared){
     const isUmb=hasTag(card,'aoe')&&!card.unique;
     const isVard=hasTag(card,'aoe')&&card.unique;
@@ -286,10 +286,10 @@ const tagIcons = (card.tags||[])
     <div class="card-name-box"><div class="card-name">${card.name}</div></div>
     ${!isSW?`<div class="card-stats">
       <div class="card-hp-box"><span class="card-hp"><img src="./img/heart.png" class="stat-icon">${card.maxHp}</span></div>
-        <div class="stat-img"></div>
+        <img src="img/chel.png" class="card-stats-icon">
       <div class="card-atk-box"><span class="card-atk"><img src="./img/attack.png" class="stat-icon">${card.atk+(card.atkBonus||0)+(card.rageBonus||0)+(card.squadAtkBonus||0)}</span></div>
     </div>`
-      :`<div class="card-stats" style="justify-content:center;"><div class="stat-img"></div>`}
+      :`<div class="card-stats" style="justify-content:center;"><img src="img/chel.png" class="card-stats-icon"></div>`}
     <div class="card-ability-box"><div class="card-ability">${card.ab}</div></div>`;
   if(card.id===G.previewCard&&zone==='hand'){
     d.classList.add('previewed');
