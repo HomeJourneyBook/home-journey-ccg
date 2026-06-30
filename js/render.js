f814a26599b083f107928bdf7b2cd4bed232e495
Home’s Journey Book <hometavern113@gmail.com>
2026-06-30 23:26:15 +0700

    Update render.js

--- js/render.js
+++ js/render.js
@@ -169,7 +169,7 @@ const tagIcons=(card.tags||[])
     <div class="card-small-name-box"><div class="card-small-name">${card.name}</div></div>
 ${!isSW?`<div class="card-small-stats">
   <div class="card-small-hp-box"><span class="card-small-hp">${card.hp}</span></div>
-  <div class="card-small-stat-img"></div>
+  <div class="stat-img"></div>
   <div class="card-small-atk-box"><span class="card-small-atk">${card.atk+(card.atkBonus||0)+(card.rageBonus||0)+(card.squadAtkBonus||0)}</span></div>
 </div>`
 :`<div class="card-small-stats" style="justify-content:center;"><div class="stat-img"></div></div>`}`;
@@ -286,7 +286,7 @@ const tagIcons = (card.tags||[])
     <div class="card-name-box"><div class="card-name">${card.name}</div></div>
     ${!isSW?`<div class="card-stats">
       <div class="card-hp-box"><span class="card-hp"><img src="./img/heart.png" class="stat-icon">${card.maxHp}</span></div>
-        <div class="card-small-stat-img"></div>
+        <div class="stat-img"></div>
       <div class="card-atk-box"><span class="card-atk"><img src="./img/attack.png" class="stat-icon">${card.atk+(card.atkBonus||0)+(card.rageBonus||0)+(card.squadAtkBonus||0)}</span></div>
     </div>`
       :`<div class="card-stats" style="justify-content:center;"><div class="stat-img"></div>`}
