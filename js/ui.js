b5adcd9ea0dc0ba5baca61410b52bd17867e1e16
Home’s Journey Book <hometavern113@gmail.com>
2026-07-01 11:26:28 +0700

    +random stars

--- js/ui.js
+++ js/ui.js
@@ -328,6 +328,9 @@ function spawnStars() {
   ['oppFieldZone','playerFieldZone'].forEach(id => {
     const zone = document.getElementById(id);
     if (!zone) return;
+    const size = [1,1,1,2,2,3][Math.floor(Math.random()*6)]; // чаще маленькие
+s.style.width  = size + 'px';
+s.style.height = size + 'px';
     for (let i = 0; i < 30; i++) {
       const s = document.createElement('div');
       s.className = 'field-star';
