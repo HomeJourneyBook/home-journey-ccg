b98e7c9fe8ca5b1f13f9e3c6ea7f411954f5ff20
Home’s Journey Book <hometavern113@gmail.com>
2026-07-02 12:22:53 +0700

    Add vanguard icon to tag iconsadd

--- js/catalog.js
+++ js/catalog.js
@@ -56,6 +56,7 @@ function renderCatalog(){
       'burn':    '<img src="img/ico_burn.png" style="width:60%;height:60%;">',
       'rage':    '<img src="img/ico_rage.png" style="width:60%;height:60%;">',
       'provoke': '<img src="img/ico_provoke.png" style="width:60%;height:60%;">',
+      'vanguard':'<img src="img/ico_vanguard.png" style="width:60%;height:60%;">',
     };
     const tagIcons=(def.tags||[])
       .map(t=>t.split(':')[0])
@@ -110,6 +111,7 @@ function openCardDetail(def){
     'burn':    '<img src="img/ico_burn.png" style="width:60%;height:60%;">',
     'rage':    '<img src="img/ico_rage.png" style="width:60%;height:60%;">',
     'provoke': '<img src="img/ico_provoke.png" style="width:60%;height:60%;">',
+    'vanguard':'<img src="img/ico_vanguard.png" style="width:60%;height:60%;">',
   };
   const tagIcons = (def.tags||[])
     .map(t=>t.split(':')[0])
