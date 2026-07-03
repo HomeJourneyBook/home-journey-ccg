3d062ed8a11b7ae9a4927cd9e73a19374496e2b6
Home’s Journey Book <hometavern113@gmail.com>
2026-07-04 02:12:36 +0700

    Add extra span elements for player and opponent stats

--- js/render.js
+++ js/render.js
@@ -735,10 +735,12 @@ function reorderZones(){
     oppStats.className='stats-bar '+(oppK==='jeet'?'jeet':'tea');
     oppStats.innerHTML=`
   ${_mkPcardSlotHtml(oppP.world, oppK, false)}
+  <span class="statbar-extra"></span>
   <span class="hp-placeholder"></span>
   <span class="stat stat-hp-box ${oppK}-hp-box"><img src="./img/hp_${oppK}.png" class="stat-icon"> <span class="stat-val hp-val" id="${oppK}Hp">${oppP.hp}</span></span>
   <span class="player-name-box ${oppK}-name-box" role="img" aria-label="${oppK==='jeet'?'JEET CORE':'TAVERN'}" onclick="event.stopPropagation();onBaseClick('${oppK}')"></span>
   <span class="stat stat-ess-box ${oppK}-ess-box"><img src="./img/ess.png" class="stat-icon"> <span class="ess-val" id="${oppK}Ess">${oppP.ess}</span>/<span id="${oppK}EssMax">${oppP.essMax}</span></span>
+  <span class="statbar-extra"></span>
   ${_mkPcardSlotHtml(oppP.artifacts[0]||null, oppK, false)}`;
     oppStats.querySelectorAll('[data-pid]').forEach(el=>{
       const pid=el.dataset.pid;
@@ -749,10 +751,12 @@ function reorderZones(){
     playerStats.className='stats-bar '+(playerK==='jeet'?'jeet':'tea');
     playerStats.innerHTML=`
   ${_mkPcardSlotHtml(playerP.world, playerK, false)}
+  <span class="statbar-extra"></span>
   <span class="hp-placeholder"></span>
   <span class="stat stat-hp-box ${playerK}-hp-box"><img src="./img/hp_${playerK}.png" class="stat-icon"> <span class="stat-val hp-val" id="${playerK}Hp">${playerP.hp}</span></span>
   <span class="player-name-box ${playerK}-name-box" role="img" aria-label="${playerK==='jeet'?'JEET CORE':'TAVERN'}" onclick="event.stopPropagation();onBaseClick('${playerK}')"></span>
   <span class="stat stat-ess-box ${playerK}-ess-box"><img src="./img/ess.png" class="stat-icon"> <span class="ess-val" id="${playerK}Ess">${playerP.ess}</span>/<span id="${playerK}EssMax">${playerP.essMax}</span></span>
+  <span class="statbar-extra"></span>
   ${_mkPcardSlotHtml(playerP.artifacts[0]||null, playerK, true)}`;
     playerStats.querySelectorAll('[data-pid]').forEach(el=>{
       const pid=el.dataset.pid;
