7024752a7fe2f05206673d2875eb75214e3b2a7f
Home’s Journey Book <hometavern113@gmail.com>
2026-07-05 23:59:06 +0700

    TEST3

--- js/deck.js
+++ js/deck.js
@@ -64,5 +64,5 @@ function mkCard(key){
   return{id:'c'+UID,key,name:def.name,cost:def.cost,hp:def.hp,maxHp:def.hp,atk:def.atk,art:def.art,img:def.img||null,
     f:def.f,tags:[...(def.tags||[])],ab:def.ab||'',
     spell:!!def.spell,world:!!def.world,artifact:!!def.artifact,unique:!!def.unique,
-    sleeping:false,exhausted:false,feared:false,burning:false,atkBonus:0};
+    sleeping:false,exhausted:false,feared:false,burning:false,atkBonus:0,tempAtkBonus:0};
 }
