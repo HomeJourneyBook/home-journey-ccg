e7f5b88069d7953a1cc8825890810256b29e1d9f
Home’s Journey Book <hometavern113@gmail.com>
2026-07-05 15:23:36 +0700

    Replace invisible icon placeholder with actual image

--- js/render.js
+++ js/render.js
@@ -233,7 +233,7 @@ function mkSmallEl(card){
   'rage':    '<img src="img/ico_rage.png" style="width:60%;height:60%;">',
   'provoke': '<img src="img/ico_provoke.png" style="width:60%;height:60%;">',
   'vanguard':'<img src="img/ico_vanguard.png" style="width:60%;height:60%;">',
-  'invisible':'<span style="font-size:80%;opacity:.9;">👻</span>', /* placeholder — swap for real ico_invisible.png once art exists */
+  'invisible':'<img src="img/ico_invis.png" style="width:60%;height:60%;">',
 };
 const tagIcons=(card.tags||[])
   .map(t=>t.split(':')[0])
@@ -391,7 +391,7 @@ function mkEl(card,zone){
   'rage':    '<img src="img/ico_rage.png" style="width:60%;height:60%;">',
   'provoke': '<img src="img/ico_provoke.png" style="width:60%;height:60%;">',
   'vanguard':'<img src="img/ico_vanguard.png" style="width:60%;height:60%;">',
-  'invisible':'<span style="font-size:80%;opacity:.9;">👻</span>', /* placeholder — swap for real ico_invisible.png once art exists */
+  'invisible':'<img src="img/ico_invis.png" style="width:60%;height:60%;">',
 };
 const tagIcons = (card.tags||[])
   .map(t=>t.split(':')[0])
