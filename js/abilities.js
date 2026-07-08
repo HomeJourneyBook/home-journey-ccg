8372cea388f30038c404b544369a5cd6d1f9fc1c
Home’s Journey Book <hometavern113@gmail.com>
2026-07-08 21:27:06 +0700

    Fix sound effect name for aoe effect

--- js/abilities.js
+++ js/abilities.js
@@ -133,7 +133,7 @@ function triggerAbilities(card, timing, ctx={}){
     switch(a.effect){
 
       case 'aoe':
-        playSfx('card_spell_atack');
+        playSfx('card_atack');
         [...G[oppK].field].forEach(t=>dmgCard(t,a.val,oppK));
         lg(`${card.name}: ${a.val} dmg to all enemies!`,'imp');
         break;
