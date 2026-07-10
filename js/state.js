939ef5363142e19c99dc412376e8e440e8f6af05
Home’s Journey Book <hometavern113@gmail.com>
2026-07-10 21:12:09 +0700

    Update resetC function to include armorMax property

--- js/state.js
+++ js/state.js
@@ -108,7 +108,7 @@ function findC(id){
 }
 
 function resetC(c){
-  c.sleeping=false;c.exhausted=false;c.feared=false;c.burning=false;c.atkBonus=0;c.rageBonus=0;c.tempAtkBonus=0;c.maxHpBonus=0;c.squadMaxHpBonus=0;c.squadAtkBonus=0;c.squadParam=null;c.armor=0;
+  c.sleeping=false;c.exhausted=false;c.feared=false;c.burning=false;c.atkBonus=0;c.rageBonus=0;c.tempAtkBonus=0;c.maxHpBonus=0;c.squadMaxHpBonus=0;c.squadAtkBonus=0;c.squadArmorBonus=0;c.squadParam=null;c.armor=0;c.armorMax=undefined;
   const def=DEFS[c.key];if(def){c.hp=def.hp;c.maxHp=def.hp;}
 }
 
