b0edf5fa6dc58258a9538fb38f1f28fb1dc2aa3a
Home’s Journey Book <hometavern113@gmail.com>
2026-07-12 11:56:56 +0700

    Change 'umb' effect from 'aoe' to 'heal'

--- js/game.js
+++ js/game.js
@@ -696,7 +696,7 @@ const SQUAD_DEFS = [
   {gtype:'drg', count:3, effect:'armor', val:1},
   {gtype:'mch', count:3, effect:'atk',   val:1},
   {gtype:'orb', count:3, effect:'param', param:'heal',   val:2},
-  {gtype:'umb', count:3, effect:'param', param:'aoe',    val:2},
+  {gtype:'umb', count:3, effect:'param', param:'heal',   val:2},
   {gtype:'szg', count:3, effect:'maxhp', val:1},
   {gtype:'xui', count:3, effect:'param', param:'regen',  val:2},
 ];
