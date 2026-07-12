6c924d2f64f8b68e060dcd17d30502683e0f1488
Home’s Journey Book <hometavern113@gmail.com>
2026-07-13 02:50:20 +0700

    Add fifth spell option for both types

--- js/deck.js
+++ js/deck.js
@@ -46,8 +46,8 @@ function _composeDeckList(f, cfg){
   const legs   = t ? ['t_tean','t_aslex','t_tuborg','t_faeron','t_nab']
                    : ['j_reap','j_ryv','j_mal','j_phleg','j_vard'];
 
-  const spells = t ? ['t_sp1','t_sp2','t_sp3','t_sp4']
-                   : ['j_sp1','j_sp2','j_sp3','j_sp4'];
+  const spells = t ? ['t_sp1','t_sp2','t_sp3','t_sp4','t_sp5']
+                   : ['j_sp1','j_sp2','j_sp3','j_sp4','j_sp5'];
 
   const worlds = t ? ['t_w1','t_w2'] : ['j_w1','j_w2'];
   const arts   = t ? ['t_a1','t_a2'] : ['j_a1','j_a2'];
