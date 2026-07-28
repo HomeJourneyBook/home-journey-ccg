99f6c585879297e6c88d0a763dd4885f4e010625
Home’s Journey Book <hometavern113@gmail.com>
2026-07-28 23:19:45 +0700

    Add 'market' icon for GAME OF MARKET

--- js/deckbuilder.js
+++ js/deckbuilder.js
@@ -88,6 +88,7 @@ const DB_TAG_ICONS = {
   'death_armor': '<img src="img/ico_scheme.png" style="width:60%;height:60%;">', // SCHEME (новый тег) — death_armor:N, 2026-07-27, занял иконку/трейт-слот после переезда death_bolt на Pink Clouds
   'frost': '<img src="img/ico_snow.png" style="width:60%;height:60%;">', // FROST ATTACK — Winter from RGB, ультраредкий Mood-трейт, 2026-07-27
   'foxy': '<img src="img/ico_fff.png" style="width:60%;height:60%;">', // FOXY TRICK — Orange from FFF, ультраредкий Mood-трейт, 2026-07-27
+  'market': '<img src="img/ico_market.png" style="width:60%;height:60%;">', // GAME OF MARKET — To the Moon with DHD, ультраредкий Mood-трейт, 2026-07-28
 };
 
 // Категории для кнопок-фильтров над правой (пул) областью — переиспользует ту же
