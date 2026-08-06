4f5651af470180d4efd001d2d7180898c33afd14
Home’s Journey Book <hometavern113@gmail.com>
2026-08-07 01:47:16 +0700

    Add shadow shield image to render.js

--- js/render.js
+++ js/render.js
@@ -805,6 +805,7 @@ function mkSmallEl(card){
   'intercept': '<img src="img/ico_intercept.png" style="width:60%;height:60%;">',
   'stealth': '<img src="img/ico_stealth.png" style="width:60%;height:60%;">',
   'thorns': '<img src="img/ico_fire_shield.png" style="width:60%;height:60%;">',
+  'shadow_shield': '<img src="img/ico_shadow.png" style="width:60%;height:60%;">',
   'shield': '<img src="img/ico_solana_shield.png" style="width:60%;height:60%;">',
   'draw_attack': '<img src="img/ico_haunt.png" style="width:60%;height:60%;">', // HAUNT — переехал с atk_vs_feared на draw_attack, 2026-07-26
   'death_heal': '<img src="img/ico_bambo.png" style="width:60%;height:60%;">', // BAMBOO — death_heal:N, 2026-07-26
@@ -1083,6 +1084,7 @@ function mkEl(card,zone){
   'intercept': '<img src="img/ico_intercept.png" style="width:60%;height:60%;">',
   'stealth': '<img src="img/ico_stealth.png" style="width:60%;height:60%;">',
   'thorns': '<img src="img/ico_fire_shield.png" style="width:60%;height:60%;">',
+  'shadow_shield': '<img src="img/ico_shadow.png" style="width:60%;height:60%;">',
   'shield': '<img src="img/ico_solana_shield.png" style="width:60%;height:60%;">',
   'draw_attack': '<img src="img/ico_haunt.png" style="width:60%;height:60%;">', // HAUNT — переехал с atk_vs_feared на draw_attack, 2026-07-26
   'death_heal': '<img src="img/ico_bambo.png" style="width:60%;height:60%;">', // BAMBOO — death_heal:N, 2026-07-26
